702e509f6873e1f0c89c241c4b72b6249e49f137
xueqingxia <962204414@qq.com>
2019-05-17 15:40:48 +0800

    说明

--- FSCAppPages/Layouts/FSCAppPages/Corpus/index.aspx.cs
+++ FSCAppPages/Layouts/FSCAppPages/Corpus/index.aspx.cs
@@ -6,8 +6,10 @@ namespace FSCAppPages.Layouts.FSCAppPages.Corpus
 {
     public partial class index : LayoutsPageBase
     {
+        #region 事件
         protected void Page_Load(object sender, EventArgs e)
         {
         }
+        #endregion
     }
 }
